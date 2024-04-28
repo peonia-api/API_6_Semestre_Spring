1212ed842cfd0018e7dee49f57c5d16486cb44a4
Jennyads <jennycristina.1998@gmail.com>
2024-04-28 20:31:23 -0300

    feat: added jwt for login authentication

--- login/src/main/java/br/gov/fatec/login/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.gov.fatec.login.repository;
-
-
-import br.gov.fatec.login.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
